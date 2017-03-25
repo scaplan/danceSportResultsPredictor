ef18772b4e988b352a67f1e74eac774d8acc762f
scaplan <github@spencercaplan.org>
2017-03-25 14:52:08 -0400

    Attempt to look for missing full entries by A)Leader Name or B)Leader's Surname

--- ballroom-results-predictor.py
+++ ballroom-results-predictor.py
@@ -7,8 +7,14 @@ sys.setdefaultencoding('utf-8')
 import unicodedata, operator
 from unicodedata import normalize
 
+fullCoupleRankingDict = {}
+leaderFullnameRankingDict = {}
+leaderSurnameRankingDict = {}
+
 def extractRankingFromFile(inputFilePath):
-	rankingDict = {}
+	global fullCoupleRankingDict, leaderFullnameRankingDict, leaderSurnameRankingDict
+	doubleEntryLeader = []
+	doubleEntryLeaderSurname = []
 	with open(inputFilePath, 'r') as inputFile:
 		for currLine in inputFile:
 			if '&' in currLine:
@@ -17,25 +23,52 @@ def extractRankingFromFile(inputFilePath):
 				rank = tokens[0][:rankIndex]
 				leaderIndex = tokens[0].rfind("\t")
 				leader = tokens[0][leaderIndex+1:].strip().lower()
+				leaderSurname = leader.split(' ')[1]
 				follow = tokens[1].strip().lower()
 				couple = leader + ' & ' + follow
-				rankingDict[couple] = int(rank)
-				#print rank + couple
-	return rankingDict
+				fullCoupleRankingDict[couple] = int(rank)
 
-def predictResults(rankingDict, eventListFilePath):
+				# So that double entries are removed at the end rather than overwritten
+				if leader in leaderFullnameRankingDict:
+					doubleEntryLeader.append(leader)
+				if leaderSurname in leaderSurnameRankingDict:
+					doubleEntryLeaderSurname.append(leaderSurname)
+
+				leaderFullnameRankingDict[leader] = int(rank)
+				leaderSurnameRankingDict[leaderSurname] = int(rank)
+				#print rank + couple
+
+	for leader in doubleEntryLeader:
+		if leader in leaderFullnameRankingDict:
+			del leaderFullnameRankingDict[leader]
+	for leaderSurname in doubleEntryLeaderSurname:
+		if leaderSurname in leaderSurnameRankingDict:
+			del leaderSurnameRankingDict[leaderSurname]
+
+def predictResults(eventListFilePath):
+	global fullCoupleRankingDict, leaderFullnameRankingDict, leaderSurnameRankingDict
 	competingDict = {}
 	with open(eventListFilePath, 'r') as eventFile:
 		for currLine in eventFile:
 			currCouple = currLine.split(' of ')[0].strip().lower()
-			if currCouple in rankingDict:
-				competingDict[currCouple] = rankingDict[currCouple]
+			currLeader = currCouple.split(' & ')[0]
+			currLeaderSurname = currLeader.split(' ')[1]
+			if currCouple in fullCoupleRankingDict:
+				competingDict[currCouple] = fullCoupleRankingDict[currCouple]
+			elif currLeader in leaderFullnameRankingDict:
+				competingDict[currCouple] = leaderFullnameRankingDict[currLeader]
+			elif currLeaderSurname in leaderSurnameRankingDict:
+				competingDict[currCouple] = leaderSurnameRankingDict[currLeaderSurname]
 			else:
-				competingDict[currCouple] = 101
+				competingDict[currCouple] = 999
 	
 	sorted_heatList = sorted(competingDict.items(), key=operator.itemgetter(1))
 	counter = 1
+	printedUnrankBreak = False
 	for couple, rank in sorted_heatList:
+		if rank == 999 and not printedUnrankBreak:
+			print '\n-----UNRANKED-----'
+			printedUnrankBreak = True
 		print str(counter) + '. ' + couple + ' --- ' + str(rank)
 		counter += 1
 
@@ -51,5 +84,5 @@ if __name__=="__main__":
 	priorRankingFile = sys.argv[1]
 	eventListFile = sys.argv[2]
 
-	rankingDict = extractRankingFromFile(priorRankingFile)
-	predictResults(rankingDict, eventListFile)
+	extractRankingFromFile(priorRankingFile)
+	predictResults(eventListFile)
